e8d10d9dae1673964ca3701bd2ec1615dd2f6504
Shidul Islam <sishahin093@gmail.com>
2022-05-14 09:56:23 +0600

    booking date load at post api creating

--- index.js
+++ index.js
@@ -19,13 +19,36 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("doctors_portal").collection("services");
+        const bookingCollection = client.db("doctors_portal").collection("bookings");
 
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const service = await cursor.toArray();
             res.send(service);
-        })
+        });
+
+        /**
+         * API Naming COnvention 
+         * app.get('booking) // get all booking in this collection or get more than one or by fillter
+         * app.get('booking/:id) // get a specific booking
+         * app.get('booking) add a new booking
+         * app.get('booking)
+         * app.get('booking)
+        */
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            // console.log("bookin", booking.treatmentName);
+            const query = { treatmentName: booking.treatmentName, date: booking.date, patient: booking.patient };
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists });
+            }
+            const result = await bookingCollection.insertOne(booking);
+            res.send({ success: true, result });
+        });
+
+
     } finally {
 
     }
