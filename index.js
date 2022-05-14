f024dfc906032b5a3fa8c3ef6918af2161eca46b
Shidul Islam <sishahin093@gmail.com>
2022-05-14 11:20:26 +0600

    74-4 (INtersting  ) FInd Available time slots for a day

--- index.js
+++ index.js
@@ -28,7 +28,51 @@ async function run() {
             res.send(service);
         });
 
-        /**
+        app.get('/available', async (req, res) => {
+
+
+            // const date = req.query.date || 'May 14, 2022';
+            // const services = await serviceCollection.find().toArray();
+
+            // const query = { date: date };
+            // const bookings = await bookingCollection.find(query).toArray();
+
+            // services.forEach(service => {
+            //     const serviceBooking = bookings.filter(b => b.treatmentName === service.name);
+            //     const booked = serviceBooking.map(s => s.slot);
+            //     const available = service.slots.filter(s => !booked(s));
+            //     service.available = available;
+
+            // })
+
+            // res.send(services);
+
+
+
+            const date = req.query.date || 'May 14, 2022';
+            // step1. get all services
+            const services = await serviceCollection.find().toArray();
+            // steps2 . get the booking of that day 
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            // sets3 . for each service , find bookings for that serive
+            services.forEach(service => {
+                const serviceBooking = bookings.filter(b => b.treatmentName === service.name);
+                const booked = serviceBooking.map(s => s.slot);
+                const available = service.slots.filter(s => !booked.includes(s));
+                service.available = available;
+            })
+            res.send(services);
+
+
+
+
+
+        })
+
+
+        /*
          * API Naming COnvention 
          * app.get('booking) // get all booking in this collection or get more than one or by fillter
          * app.get('booking/:id) // get a specific booking
