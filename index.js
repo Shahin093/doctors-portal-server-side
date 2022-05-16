280eeb2ea4d71393c20f2995ccb154c351724ac8
Shidul Islam <sishahin093@gmail.com>
2022-05-16 13:12:11 +0600

    update data to server root

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
         await client.connect();
         const serviceCollection = client.db("doctors_portal").collection("services");
         const bookingCollection = client.db("doctors_portal").collection("bookings");
+        const userCollection = client.db("doctors_portal").collection("users");
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -28,6 +29,18 @@ async function run() {
             res.send(service);
         });
 
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const user = req.body;
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
 
         // warning
         // THis is not the Proper way to query . 
