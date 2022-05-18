64b105ec8a6a7f1b26493ae13356168b4efee646
Shidul Islam <sishahin093@gmail.com>
2022-05-18 14:46:56 +0600

    load data admin page

--- index.js
+++ index.js
@@ -64,6 +64,12 @@ async function run() {
             res.send(service);
         });
 
+        app.get('/user', async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+
+        })
+
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
